266016d1a730dc3a7f5a291581746d95a8dcea3b
idolhand <55949857+idolhand@users.noreply.github.com>
2019-09-29 16:01:23 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,47 @@
-alert("BOOP");
+(function() {
+    var canvas = document.getElementById('canvas'),
+            context = canvas.getContext('2d');
+  
+  // zdog-demo.js
+
+// create illo
+let illo = new Zdog.Illustration({
+  // set canvas with selector
+  element: '.zdog-canvas',
+});
+
+// add circle
+new Zdog.Ellipse({
+  addTo: illo,
+  diameter: 80,
+  stroke: 20,
+  color: '#636',
+});
+
+// update & render
+illo.updateRenderGraph();
+
+    // resize the canvas to fill browser window dynamically
+    window.addEventListener('resize', resizeCanvas, false);
+
+    function resizeCanvas() {
+            canvas.width = window.innerWidth;
+            canvas.height = window.innerHeight;
+
+            /**
+             * Your drawings need to be inside this function otherwise they will be reset when 
+             * you resize the browser window and the canvas goes will be cleared.
+             */
+            drawStuff(); 
+    }
+    resizeCanvas();
+
+    function drawStuff() {
+            // do your drawing stuff here
+      // rotate illo each frame
+  illo.rotate.y += 0.03;
+  illo.updateRenderGraph();
+  // animate next frame
+  requestAnimationFrame( animate );
+    }
+})();
