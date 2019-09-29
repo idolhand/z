e19582841986ea5920b1b2290a9422d07044dcef
idolhand <55949857+idolhand@users.noreply.github.com>
2019-09-29 16:08:50 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-(function() {
-    var canvas = document.getElementById('canvas'),
-            context = canvas.getContext('2d');
-  
-  // zdog-demo.js
+// zdog-demo.js
 
 // create illo
 let illo = new Zdog.Illustration({
@@ -20,28 +16,3 @@ new Zdog.Ellipse({
 
 // update & render
 illo.updateRenderGraph();
-
-    // resize the canvas to fill browser window dynamically
-    window.addEventListener('resize', resizeCanvas, false);
-
-    function resizeCanvas() {
-            canvas.width = window.innerWidth;
-            canvas.height = window.innerHeight;
-
-            /**
-             * Your drawings need to be inside this function otherwise they will be reset when 
-             * you resize the browser window and the canvas goes will be cleared.
-             */
-            drawStuff(); 
-    }
-    resizeCanvas();
-
-    function drawStuff() {
-            // do your drawing stuff here
-      // rotate illo each frame
-  illo.rotate.y += 0.03;
-  illo.updateRenderGraph();
-  // animate next frame
-  requestAnimationFrame( drawStuff );
-    }
-})();
